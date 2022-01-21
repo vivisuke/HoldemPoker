29ce258f87891512dbc22de737176e439457f203
vivisuke <ntsuda@master.email.ne.jp>
2022-01-21 13:32:30 +0900

    0024 ◎ C 01/20 01/21 0.001│ToDo│RANK_2 = 0, ... RANK_A = 12 に変更

--- HoldemPoker/scripts/Card.gd
+++ HoldemPoker/scripts/Card.gd
@@ -1,16 +1,19 @@
 extends Sprite
 
-const NumTable = "A234567890JQK"
+#const NumTable = "A234567890JQK"
+const RANK_10 = 10 - 2
+const NumTable = "234567890JQKA"
+
 func _ready():
 	pass # Replace with function body.
 
 func set_suit(st):
 	$Suit.set_frame(st)
 func set_rank(rank):
-	if rank == 10:
+	if rank == RANK_10:
 		$Label.text = "10"
 	else:
-		$Label.text = NumTable[rank-1]
+		$Label.text = NumTable[rank]
 func set_sr(st, rank):
 	set_suit(st)
 	set_rank(rank)
--- HoldemPoker/scripts/MainScene.gd
+++ HoldemPoker/scripts/MainScene.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
-const N_SUIT = 4
-const N_RANK = 13
+#enum { CLUB = 0, DIAMOND, HEART, SPADE, }
+enum {
+	CLUBS = 0, DIAMONDS, HEARTS, SPADES, N_SUIT,
+	RANK_BLANK = -1,
+	RANK_2 = 0, RANK_3, RANK_4, RANK_5, RANK_6,
+	RANK_7, RANK_8, RANK_9, RANK_10,
+	RANK_J, RANK_Q, RANK_K, RANK_A, N_RANK,
+}
+#const N_SUIT = 4
+#const N_RANK = 13
 const N_CARDS = N_RANK*N_SUIT
-
-enum { CLUB = 0, DIAMOND, HEART, SPADE, }
 
 var deck = []		# 要素：(suit << 4) | rank
 
@@ -16,16 +22,16 @@ func _ready():
 		deck
 	#
 	$PlayerBG1.set_name("vivisuke")
-	$PlayerBG1.set_card1(SPADE, 1)
-	$PlayerBG1.set_card2(SPADE, 13)
+	$PlayerBG1.set_card1(SPADES, RANK_A)
+	$PlayerBG1.set_card2(SPADES, RANK_K)
 	$PlayerBG1.set_BG(1)
 	$PlayerBG2.set_BG(2)
 	#
-	$Table/Card1.set_sr(CLUB, 1)
-	$Table/Card2.set_sr(DIAMOND, 5)
-	$Table/Card3.set_sr(HEART, 10)
-	$Table/Card4.set_sr(SPADE, 12)
-	$Table/Card5.set_sr(HEART, 13)
+	$Table/Card1.set_sr(CLUBS, RANK_A)
+	$Table/Card2.set_sr(DIAMONDS, RANK_5)
+	$Table/Card3.set_sr(HEARTS, RANK_10)
+	$Table/Card4.set_sr(SPADES, RANK_Q)
+	$Table/Card5.set_sr(HEARTS, RANK_K)
 	#
 	#$PlayerBG1.open_cards()
 	pass
