94fd2bd8c4635d56640aa5197f7be566bea876e4
vivisuke <ntsuda@master.email.ne.jp>
2022-01-22 06:19:37 +0900

    0027 ◎ C 01/21 01/22 0.001│ToDo│PlayerBG1～6 を配列に格納

--- HoldemPoker/scripts/MainScene.gd
+++ HoldemPoker/scripts/MainScene.gd
@@ -11,35 +11,59 @@ enum {
 #const N_SUIT = 4
 #const N_RANK = 13
 const N_CARDS = N_RANK*N_SUIT
+const RANK_MASK = 0x0f
+const N_RANK_BITS = 4
 
 var deck = []		# 要素：(suit << 4) | rank
+var players = []	# プレイヤーパネル配列、[0] for Human
+var nPlayers = 6	# 6 players
 
 func _ready():
+	randomize()
 	deck.resize(N_CARDS)
 	for i in range(N_CARDS):
-		var st = i / N_RANK
-		var rank = i % N_RANK + 1
-		deck
+		var st : int = i / N_RANK
+		var rank : int = i % N_RANK
+		deck[i] = (st<<N_RANK_BITS) | rank
+	deck.shuffle()
 	#
-	$PlayerBG1.set_name("vivisuke")
-	$PlayerBG1.set_card1(SPADES, RANK_A)
-	$PlayerBG1.set_card2(SPADES, RANK_K)
-	$PlayerBG1.set_BG(1)
-	$PlayerBG2.set_BG(2)
+	players = []
+	for i in range(nPlayers):
+		var pb = get_node("Table/PlayerBG%d" % (i+1))
+		players.push_back(pb)
 	#
-	$Table/Card1.set_sr(CLUBS, RANK_A)
-	$Table/Card2.set_sr(DIAMONDS, RANK_5)
-	$Table/Card3.set_sr(HEARTS, RANK_10)
-	$Table/Card4.set_sr(SPADES, RANK_Q)
-	$Table/Card5.set_sr(HEARTS, RANK_K)
+	var ix = 0
+	for i in range(nPlayers):
+		var st : int = deck[ix] >> N_RANK_BITS
+		var rank : int = deck[ix] & RANK_MASK
+		ix += 1
+		players[i].set_card1(st, rank)
+	for i in range(nPlayers):
+		var st : int = deck[ix] >> N_RANK_BITS
+		var rank : int = deck[ix] & RANK_MASK
+		ix += 1
+		players[i].set_card2(st, rank)
+	#
+	$Table/PlayerBG1.set_name("vivisuke")
+	#Table/$PlayerBG1.set_card1(SPADES, RANK_A)
+	#$Table/PlayerBG1.set_card2(SPADES, RANK_K)
+	$Table/PlayerBG1.set_BG(1)
+	$Table/PlayerBG2.set_BG(2)
+	#
+	#$Table/Card1.set_sr(CLUBS, RANK_A)
+	#$Table/Card2.set_sr(DIAMONDS, RANK_5)
+	#$Table/Card3.set_sr(HEARTS, RANK_10)
+	#$Table/Card4.set_sr(SPADES, RANK_Q)
+	#$Table/Card5.set_sr(HEARTS, RANK_K)
 	#
 	#$PlayerBG1.open_cards()
 	pass
 
 func _input(event):
 	if event is InputEventMouseButton:
-		$PlayerBG1.open_cards()
-		
+		#$PlayerBG1.open_cards()
+		for i in range(nPlayers):
+			players[i].open_cards()
 
 func _process(delta):
 	pass
