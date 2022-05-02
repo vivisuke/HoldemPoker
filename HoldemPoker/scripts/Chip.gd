f9a454518b69e3c70104c0050681ff380ddaa4cb
vivisuke <ntsuda@master.email.ne.jp>
2022-05-03 06:07:26 +0900

    0213 ◎ C 05/03 05/03 0.001│ToDo│Chipシーン：移動メソッド追加

---
+++ HoldemPoker/scripts/Chip.gd
@@ -0,0 +1,26 @@
+extends Sprite
+
+var moving = false
+var move_dur = 0.0				# 移動所要時間（単位：秒）
+var move_elapsed = 0.0			# 移動経過時間（単位：秒）
+var src_pos = Vector2(0, 0)
+var dst_pos = Vector2(0, 0)
+
+func _ready():
+	pass # Replace with function body.
+#
+func move_to(dst : Vector2, dur : float):
+	src_pos = get_position()
+	dst_pos = dst
+	move_dur = dur
+	move_elapsed = 0.0
+	moving = true
+func _process(delta):
+	if moving:		# 移動処理中
+		move_elapsed += delta	# 経過時間
+		move_elapsed = min(move_elapsed, move_dur)	# 行き過ぎ防止
+		var r = move_elapsed / move_dur				# 位置割合
+		set_position(src_pos * (1.0 - r) + dst_pos * r)		# 位置更新
+		if move_elapsed == move_dur:		# 移動終了の場合
+			moving = false
+			emit_signal("move_finished")	# 移動終了シグナル発行
--- HoldemPoker/scripts/MainScene.gd
+++ HoldemPoker/scripts/MainScene.gd
@@ -194,6 +194,8 @@ func _ready():
 	update_d_SB_BB()	# D, SB, BB マーク設置
 	#update_title_text()
 	update_roundLabel()
+	#
+	$Chip.move_to(Vector2(10, 10), 2.0)		# Test
 	pass
 func update_roundLabel():
 	$RoundLabel.text = stateText[state]
@@ -580,7 +582,7 @@ func _process(delta):
 				else:
 					var max_raise = max_raise_chips(nix)
 					if nix == USER_IX:
-						players[USER_IX].set_scale(Vector2(2.0, 2.0))
+						#players[USER_IX].set_scale(Vector2(2.0, 2.0))
 						if bet_chips_plyr[USER_IX] < bet_chips:
 							act_buttons[CHECK_CALL].text = "Call %d" % (bet_chips - bet_chips_plyr[USER_IX])
 						else:
