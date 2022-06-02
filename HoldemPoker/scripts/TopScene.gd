481e85797673c30e92ed5a06ab072855fbfeb4f6
vivisuke <ntsuda@master.email.ne.jp>
2022-06-03 04:27:52 +0900

    0383 ◎ C 06/03 06/03 0.001│ToDo│トップシーン：RiverOnly ボタン追加

--- HoldemPoker/scripts/TopScene.gd
+++ HoldemPoker/scripts/TopScene.gd
@@ -36,6 +36,9 @@ func _on_Button2_pressed():
 func _on_Button3_pressed():
 	get_tree().change_scene("res://3PKuhnPokerScene.tscn")
 	pass # Replace with function body.
+func _on_Button4_pressed():
+	get_tree().change_scene("res://RiverOnlyScene.tscn")
+	pass # Replace with function body.
 
 
 func _on_UserNameEdit_text_changed(new_text):
@@ -49,5 +52,3 @@ func _on_UserNameEdit_text_entered(new_text):
 	#g.saved_data[g.KEY_USER_NAME] = new_text
 	#g.auto_save()
 	pass # Replace with function body.
-
-
